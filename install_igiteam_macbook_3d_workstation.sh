2d21e963df91e117692bc76bbadee28527b871fb
Gabriel May <gabrielmay@Gabriels-MacBook-Pro-2.local>
2026-09-19 18:51:56 +0100

    fixes

---
+++ install_igiteam_macbook_3d_workstation.sh
@@ -0,0 +1,52 @@
+git clone "https://github.com/igiteam/igi1-macbook-3d-workstation.git"
+
+macosx_intel
+├── AnyToISO_3.9.6_f.dmg
+├── Final Draft 13 13.4.0 [].dmg
+├── Firefox 156.0b2.dmg
+├── Logic_Pro_X_10_7_9_MAS_.iso
+├── README.md
+├── TheUnarchiver.dmg
+├── VSCode-darwin-universal.dmg
+├── gfxCardStatus-2.5.zip
+├── macos-bigsur-11
+│   └── mumble_client-1.4.287.x64.dmg
+├── macos-sonoma-14
+│   ├── swift-5.10.1-RELEASE-osx.pkg
+│   └── swiftly.pkg
+├── mumble_client-1.5.901.x64.dmg
+├── obs-studio-32.2.2-macos-intel.dmg
+└── smcfancontrol_2_6.zip
+
+windows_x86
+├── AnyToISO Professional 3.9.7 Build 680 - x86
+└── PowerISO.9.2.Retail
+
+Applications/
+WineKiller
+Wineskin Winery
+PDF-To-Flipbook
+SSD Monitor
+smcFanControl
+OBS
+MXFlowSwitch
+MXKeysMini
+Mumble
+gfxCardStatus
+Firefox Developer Edition
+Final Draft 13
+Logic Pro
+The Unarchiver
+
+Users/username/Application/Wineskin/
+WS11WineCX20.0.4.app
+
+CLI 
+brew
+tree
+wine-stable
+cmake
+python
+netlify-cli
+npm
+node
